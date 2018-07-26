fed10720e20bccdfcec977fab27d37ce682d063f
kevinccbsg <kevinccbsg@gmail.com>
2018-07-26 14:40:21 +0200

    Dinamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Link from 'next/link'
 import Layout from '../comp/MyLayout'
-
+import PostLink from '../comp/PostLink'
 
 // The only requirement for components placed inside a Link
 // is they should accept an onClick prop.
 
 const Index = () => (
   <Layout>
-    <p>Hello world</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
   </Layout>
 );
 
