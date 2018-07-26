bb04b47cca021d726e72928f3c169a32d3d5ac3e
kevinccbsg <kevinccbsg@gmail.com>
2018-07-26 14:57:08 +0200

    clean urls client side

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import PostLink from '../comp/PostLink'
 // The only requirement for components placed inside a Link
 // is they should accept an onClick prop.
 
+
 const Index = () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js"/>
-      <PostLink title="Learn Next.js is awesome"/>
-      <PostLink title="Deploy apps with Zeit"/>
+      <PostLink id="hello-nextjs" title="Hello Next.js"/>
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
     </ul>
   </Layout>
 );
