c4296d74628bb5a535314efe7504c2f3b1d0519b
kevinccbsg <kevinccbsg@gmail.com>
2018-07-27 20:51:39 +0200

    Detail page with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
+import fetch from 'isomorphic-unfetch'
+import Link from 'next/link'
 import Layout from '../comp/MyLayout'
-import PostLink from '../comp/PostLink'
 
 // The only requirement for components placed inside a Link
 // is they should accept an onClick prop.
 
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js"/>
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
+    
   </Layout>
 );
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
 
 export default Index;
